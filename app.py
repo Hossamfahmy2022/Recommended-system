b37fbceae592688a8c52b51585be05f16a4f0391
Hossam Ahmed Fahmy <93485412+Hossamfahmy2022@users.noreply.github.com>
2024-02-03 17:28:32 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+dataframe = pd.read_csv("Clustered_data.csv")
+
+def Get_Recommendation(User , dataframe , num_of_recommendations):
+    Cluster = dataframe[dataframe["customer_id"] == str(User)].reset_index()["Cluster"][0]
+    df_cluster = dataframe[dataframe["Cluster"] == Cluster]
+    df_cluster = df_cluster.groupby("product_category_name")["Monetary"].sum().sort_values(ascending = False).head(int(num_of_recommendations))
+    for index , name in enumerate(df_cluster.index) : 
+        print(f"Recommendation number {index + 1} is {name}")
+def main():
+    st.title("Target Offers")
+    User = str(st.text_input("User ID"))
+    num_of_recommendations = int(st.text_input("Num of Recommendations"))
+    if st.button("Recommend"):
+        Get_Recommendation(User , dataframe , num_of_recommendations)
+
+main()
